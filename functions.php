07c186c9a3a48371714480662ddd980248bdd6d8
BrianKThomas <kanaaibrian25@gmail.com>
2021-09-27 21:12:04 +0300

    roboto to nunito

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function kanai_enqueue_resources()
     wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
     wp_enqueue_style('animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
     wp_enqueue_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.css');
-    wp_enqueue_style('roboto', 'https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+    wp_enqueue_style('nunito', 'https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap');
     wp_enqueue_style('poppins', 'https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
     wp_enqueue_style('montserrant', 'https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
     wp_enqueue_style('main', get_template_directory_uri() . '/src/css/main.css');
