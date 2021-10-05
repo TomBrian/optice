deb62f22c748f7b5aa1f6675612b10779f2d8b44
BrianKThomas <kanaaibrian25@gmail.com>
2021-10-05 11:03:30 +0300

    #$%^&*()

--- footer.php
+++ footer.php
@@ -1,11 +1,4 @@
-<footer class="theme-footer">
-    <?php
-    $short_code = get_theme_mod('footer_shortcode', '[no_code]');
-    if ($short_code != '[no_code]') {
-        echo do_shortcode($short_code);
-    }
-    ?>
-</footer>
+
 <?php wp_footer() ?>
 </body>
 
