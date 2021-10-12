201d4036bfb7e0872be9be5f74c4c6f7268b2518
BrianKThomas <kanaaibrian25@gmail.com>
2021-10-12 11:42:18 +0300

    !@#$%^&*()_+

--- templates/contact-form.php
+++ templates/contact-form.php
@@ -20,7 +20,7 @@
       </div>
     </div>
     <div class="input-wrapper my-3 flex">
-      <input type="tel" name="phone" placeholder="your phone number *" id="phone" class="form-control" required/>
+      <input type="tel" name="phone" placeholder="your phone number *" id="phone" class="form-control" required />
       <div class="icon">
         <i class="fas fa-phone"></i>
       </div>
@@ -48,7 +48,7 @@
             Search Engine Optimization
           </label>
         </div>
-        
+
         <div class="form-check">
           <label class="form-check-label">
             <input type="checkbox" class="form-check-input services" id="content" value="Content writing">
@@ -67,7 +67,7 @@
     <div class="alert alert-success">
     </div>
     <div class="alert alert-danger"></div>
-     <div class="submit-wrapper flex justify-content-center">
+    <div class="submit-wrapper flex justify-content-center">
       <button class="btn submit g-recaptcha" data-sitekey="6LdvDI4cAAAAAE3WmPg0Xb__lFVtn9y4qks-QvN2" data-callback='onSubmit' type="submit">
         Submit your request
       </button>
@@ -75,32 +75,32 @@
   </form>
 </div>
 <script>
-function onSubmit(token) {
-         if (window.grecaptcha.getResponse().length == 0){
-           location.reload();
-         }
-         else{
-    jQuery(($) => {
-            var name =  $('input[id="name"]').val();
-            var email=  $('input[name="email"]').val();
-            var phone=   $('input[id="phone"]').val();
-            var company=   $('input[id="company"]').val();
-            var message=   $('input[id="message"]').val();
-            var serviceArray = [];
-            var servicestring = '';
-            $("#protected .services:checked").each(function(){
-           serviceArray.push($(this).val());
-            });
-            serviceArray.forEach(service => {
-              servicestring += service+',';
-            });
-            $('input[name="services"]').val(servicestring);
-            console.log(servicestring);
-            if (servicestring == '') {
-         $('.allow-focus').css({'border':'solid 0.4px red'})
-        }
-         else{
-          $('#protected').css('animation','loading 2s linear infinite');
+  function onSubmit(token) {
+    if (window.grecaptcha.getResponse().length == 0) {
+      location.reload();
+    } else {
+      jQuery(($) => {
+        var name = $('input[id="name"]').val();
+        var email = $('input[name="email"]').val();
+        var phone = $('input[id="phone"]').val();
+        var company = $('input[id="company"]').val();
+        var message = $('input[id="message"]').val();
+        var serviceArray = [];
+        var servicestring = '';
+        $("#protected .services:checked").each(function() {
+          serviceArray.push($(this).val());
+        });
+        serviceArray.forEach(service => {
+          servicestring += service + ',';
+        });
+        $('input[name="services"]').val(servicestring);
+        console.log(servicestring);
+        if (servicestring == '') {
+          $('.allow-focus').css({
+            'border': 'solid 0.4px red'
+          })
+        } else {
+          $('#protected').css('animation', 'loading 2s linear infinite');
           //  var data =  $('#protected').serialize(); //serializing the form and turning it into a string
           // var data =  
           // JSON.stringify({
@@ -112,27 +112,29 @@ function onSubmit(token) {
           //   message:message
           //  });  
           var data = $('#protected').serialize();
-           var formData = new FormData; //
-           formData.append('action','entry');
-           formData.append('entry',data);
-            $.ajax("<?php echo admin_url('admin-ajax.php')?>",{
-                type: "POST",
-                data: formData,
-                contentType: false,
-                processData:false,
-                processData:false,
-                success: function(response) {
-                    $('#protected .alert-success').show();
-                    $('#protected .alert-success').text('your email has been sent.We will reply through '+response.data+' as soon as we can');
-                    $('#protected').css('animation','none');
-                },
-                error: function(response) {
-                    $('#protected .alert-danger').show();
-                    $('#protected .alert-danger').text("could not send the enquiry.please try again later");
-                    $('#protected').css('animation','none');
-                }
-            }); 
-    }})
-         }}
-      //  } ;
- </script>
+          var formData = new FormData; //
+          formData.append('action', 'entry');
+          formData.append('entry', data);
+          $.ajax("<?php echo admin_url('admin-ajax.php') ?>", {
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            processData: false,
+            success: function(response) {
+              $('#protected .alert-success').show();
+              $('#protected .alert-success').text('your email has been sent.We will reply through ' + response.data + ' as soon as we can');
+              $('#protected').css('animation', 'none');
+            },
+            error: function(response) {
+              $('#protected .alert-danger').show();
+              $('#protected .alert-danger').text("could not send the enquiry.please try again later");
+              $('#protected').css('animation', 'none');
+            }
+          });
+        }
+      })
+    }
+  }
+  //  } ;
+</script>
